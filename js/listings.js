a85c66248c090ba82bf94a2c99ffa322511e4810
Caleb MacDonald <calebmacdonald@mbp.(none)>
2011-07-06 13:47:06 -0500

    working on the selector code

--- js/listings.js
+++ js/listings.js
@@ -1,81 +1,169 @@
 $(function(){
-  $("#show-bedrooms").change(function(e){
+  $(".home-selector").change(function(e){
     var target   = $(e.target),
         value    = target.val(),
-        selector =  value === "" ? "[data-bedrooms]" : "[data-bedrooms="+value+"]",
+        value_bedroom = $("#show-bedrooms").val(),
+        value_price = $("#show-price").val(),
+        value_location = $("#show-location").val(),
+        value_property = $("#show-property").val();
+        selectorBedroom =  value_bedroom === "" ? "[data-bedrooms]" : "[data-bedrooms="+value_bedroom+"]",
+        selectorPrice =  value_price === "" ? "[data-price-range]" : "[data-price-range="+value_price+"]",
+        selectorProperty =  value_property === "" ? "[data-type]" : "[data-type="+value_property+"]",
+        selectorLocation =  value_location === "" ? "[data-location]" : "[data-location="+value_location+"]",
         listings = $('.listing'); 
     
-    // console.log(value,selector);
+    console.log(value,selectorBedroom,selectorPrice,selectorProperty,selectorLocation);
     
-    listings.
-      not(selector).
-      hide();
+    // listings.
+    //   not(selectorBedroom,selectorPrice,selectorProperty,selectorLocation).
+    //   hide();
+    //   
+    // listings.
+    //   filter(selectorBedroom,selectorPrice,selectorProperty,selectorLocation).
+    //   show();
       
-    listings.
-       filter(selector).
-       show();
-  })
-});
-
-
-$(function(){
-  $("#show-price").change(function(e){
-    var target   = $(e.target),
-        value    = target.val(),
-        selector =  value === "" ? "[data-price-range]" : "[data-price-range="+value+"]",
-        listings = $('.listing'); 
-    
-    // console.log(value,selector);
-    
-    listings.
-      not(selector).
-      hide();
-      
-    listings.
-       filter(selector).
-       show();
-  })
-});
-
-
-$(function(){
-  $("#show-location").change(function(e){
-    var target   = $(e.target),
-        value    = target.val(),
-        selector =  value === "" ? "[data-location]" : "[data-location="+value+"]",
-        listings = $('.listing'); 
-    
-    // console.log(value,selector);
-    
-    listings.
-      not(selector).
-      hide();
-      
-    listings.
-       filter(selector).
-       show();
-  })
-});
-
-
-$(function(){
-    $("#show-property").change(function(e){
-      var target   = $(e.target),
-          value    = target.val(),
-          selector =  value === "" ? "[data-type]" : "[data-type="+value+"]",
-          listings = $('.listing'); 
-  
-      // console.log(value,selector);
-  
+    if (value == "1-bedroom") {
       listings.
-        not(selector).
+        not(selectorBedroom).
         hide();
-  
       listings.
-         filter(selector).
-         show();
-    })
+        filter(selectorBedroom).
+        show();
+        }
+    if (value == "2-bedroom") {
+      listings.
+        not(selectorBedroom).
+        hide();
+      listings.
+        filter(selectorBedroom).
+        show();
+        }
+    if (value == "3-bedroom") {
+      listings.
+        not(selectorBedroom).
+        hide();
+      listings.
+        filter(selectorBedroom).
+        show();
+        }
+    if (value == "4-bedroom") {
+      listings.
+        not(selectorBedroom).
+        hide();
+      listings.
+        filter(selectorBedroom).
+        show();
+        }  
+    if (value == "3-bedroom") {
+      listings.
+        not(selectorBedroom).
+        hide();
+      listings.
+        filter(selectorBedroom).
+        show();
+        }
+    if (value == "0-150k") {
+      listings.
+        not(selectorPrice).
+        hide();
+      listings.
+        filter(selectorPrice).
+        show();
+        }
+    if (value == "150-200k") {
+      listings.
+        not(selectorPrice).
+        hide();
+      listings.
+        filter(selectorPrice).
+        show();
+        }      
+    if (value == "200-350k") {
+      listings.
+        not(selectorPrice).
+        hide();
+      listings.
+        filter(selectorPrice).
+        show();
+        }     
+    if (value == "350k-plus") {
+      listings.
+        not(selectorPrice).
+        hide();
+      listings.
+        filter(selectorPrice).
+        show();
+        }        
+    if (value == "split-level") {
+      listings.
+        not(selectorProperty).
+        hide();
+      listings.
+        filter(selectorProperty).
+        show();
+        }    
+    if (value == "townhouse") {
+      listings.
+        not(selectorProperty).
+        hide();
+      listings.
+        filter(selectorProperty).
+        show();
+        }
+    if (value == "bungalow") {
+      listings.
+        not(selectorProperty).
+        hide();
+      listings.
+        filter(selectorProperty).
+        show();
+        }
+    if (value == "condo") {
+      listings.
+        not(selectorProperty).
+        hide();
+      listings.
+        filter(selectorProperty).
+        show();
+        }
+    if (value == "osbourne") {
+      listings.
+        not(selectorLocation).
+        hide();
+      listings.
+        filter(selectorLocation).
+        show();
+        }
+    if (value == "st-vital") {
+      listings.
+        not(selectorLocation).
+        hide();
+      listings.
+        filter(selectorLocation).
+        show();
+        }
+    if (value == "river-heights") {
+      listings.
+        not(selectorLocation).
+        hide();
+      listings.
+        filter(selectorLocation).
+        show();
+        }
+    if (value == "transcona") {
+      listings.
+        not(selectorLocation).
+        hide();
+      listings.
+        filter(selectorLocation).
+        show();
+        }
+    
+    
+    
+  })
 });
+
 
 
 $(function(){
@@ -86,6 +174,8 @@ $(function(){
     $("#show-price").val("");
     $("#show-location").val("");
     $("#show-property").val("");
-    $("#listings").show();
+    $("#listings").
+      filter().
+      show();
   })
 });
