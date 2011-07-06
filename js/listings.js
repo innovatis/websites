58019e38437c314cdacf628754b45caa4c5eb009
Caleb MacDonald <calebmacdonald@mbp.(none)>
2011-07-06 10:16:49 -0500

    selectors working independently

--- js/listings.js
+++ js/listings.js
@@ -1,8 +1,8 @@
 $(function(){
-  $("#show-price").change(function(e){
+  $("#show-location").change(function(e){
     var target   = $(e.target),
         value    = target.val(),
-        selector =  value === "" ? "[data-price-range]" : "[data-price-range="+value+"]",
+        selector =  value === "" ? "[data-location]" : "[data-location="+value+"]",
         listings = $('.listing'); 
     
     console.log(value,selector);
@@ -15,4 +15,59 @@ $(function(){
        filter(selector).
        show();
   })
+  
+
+  // $(function(){
+  //   $("#show-property").change(function(e){
+  //     var target   = $(e.target),
+  //         value    = target.val(),
+  //         selector =  value === "" ? "[data-type]" : "[data-type="+value+"]",
+  //         listings = $('.listing'); 
+  // 
+  //     console.log(value,selector);
+  // 
+  //     listings.
+  //       not(selector).
+  //       hide();
+  // 
+  //     listings.
+  //        filter(selector).
+  //        show();
+  //   })
+  
+  
+  // $("#show-bedrooms").change(function(e){
+  //   var target   = $(e.target),
+  //       value    = target.val(),
+  //       selector =  value === "" ? "[data-bedrooms]" : "[data-bedrooms="+value+"]",
+  //       listings = $('.listing'); 
+  //   
+  //   console.log(value,selector);
+  //   
+  //   listings.
+  //     not(selector).
+  //     hide();
+  //     
+  //   listings.
+  //      filter(selector).
+  //      show();
+  // })
+  
+  
+  // $("#show-price").change(function(e){
+  //     var target   = $(e.target),
+  //         value    = target.val(),
+  //         selector =  value === "" ? "[data-price-range]" : "[data-price-range="+value+"]",
+  //         listings = $('.listing'); 
+  //     
+  //     console.log(value,selector);
+  //     
+  //     listings.
+  //       not(selector).
+  //       hide();
+  //       
+  //     listings.
+  //        filter(selector).
+  //        show();
+  //   })
 });
