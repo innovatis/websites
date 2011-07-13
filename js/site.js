f93a914c3fc89237d680f8269d78a996a66793d8
Tim Herd <erauqssidlroweht@eqdw.net>
2011-07-13 10:18:58 -0500

    finished per-room pages

--- js/site.js
+++ js/site.js
@@ -1,10 +1,8 @@
 $(function(){
 
-  $("#main-menu .main-nav > li").live("click", function(e){
+  $("#main-menu .main-nav > li a").live("click", function(e){
     var menuButton = $(e.target).closest('li');
-    console.log("in handler");
     if(menuButton.hasClass("menu-open")){
-      console.log("has class");
       menuButton.find(".dropdown").slideUp(function(){
         menuButton.removeClass("menu-open");
       });
@@ -12,7 +10,7 @@ $(function(){
       menuButton.addClass("menu-open");
       menuButton.find(".dropdown").slideDown();
     }
-
+    return false;
   });
 
 });
