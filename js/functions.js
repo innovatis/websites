2e991b91f609db5a2bcd8ee5b2237135a54f4c02
Caleb MacDonald <cmacdonald@innovatisinc.ca>
2011-07-27 11:44:41 -0500

    slider working, buttons working, loop working

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,17 @@
 $(function() {
-	$(".scrollable").scrollable();
-	$("#browsable").scrollable().navigator();
+	$(".scrollable").scrollable({circular:true});
+	
+	
+	$(".scrollable").scrollable().onSeek(function(e,index){
+	  $('[data-seek-to]').removeClass("current");
+	  $('[data-seek-to]').eq(index).addClass("current");  
+	});
+	
+	$('[data-seek-to]').click(function(){
+	  var element = $(this),
+	           id = element.attr('data-seek-to'),
+	          api = $('.scrollable').scrollable();
+	          
+	 api.seekTo(id);
+  });
 });
