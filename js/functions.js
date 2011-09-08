f6fa76b67a38dd1f25c940e02be76b5e85d83cd5
Caleb MacDonald <cmacdonald@innovatisinc.ca>
2011-09-08 14:14:30 -0500

    slideshow functional

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,3 @@
 $(function() {
-	$(".scrollable").scrollable({circular:true});
-	
-	
-	$(".scrollable").scrollable().onSeek(function(e,index){
-	  $('[data-seek-to]').removeClass("current");
-	  $('[data-seek-to]').eq(index).addClass("current");  
-	});
-	
-	$('[data-seek-to]').click(function(){
-	  var element = $(this),
-	           id = element.attr('data-seek-to'),
-	          api = $('.scrollable').scrollable();
-	          
-	 api.seekTo(id);
-  });
+	$(".scrollable").scrollable();
 });
