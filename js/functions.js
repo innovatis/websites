98ec53e62bd898f068ad8f9d8a866a2e30968910
Caleb MacDonald <cmacdonald@innovatisinc.ca>
2011-09-09 16:06:55 -0500

    added waypoints to products page

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,48 @@
+
+var productWaypoint = function(event,direction){
+    event.stopPropagation();
+    
+    var element = $(this),
+    navHolder   = $('#sticky-nav');
+    
+    if(direction === 'down'){
+      navHolder.addClass("sticky");
+    }else{
+      navHolder.removeClass("sticky");
+    }
+    
+  };
+  
+var footerWaypoint = function(event,direction){
+  event.stopPropagation();
+  
+  var element = $(this),
+  navHolder   = $('#sticky-nav');
+  if(direction === 'down'){
+      navHolder.hide();
+  }else{
+      navHolder.show();
+  }
+  
+  
+};
+  
 $(function() {
-	$(".scrollable").scrollable();
+  var scrollable     = $('.scrollable'),
+      products       = $('.products'),
+      header         = $('#header'),
+      footerProducts = $('#products'),
+      navHolder      = $('#sticky-nav');
+
+      
+  scrollable.scrollable();
+  
+	$.waypoints.settings.scrollThrottle = 30;
+	
+	products.waypoint(productWaypoint);
+	footerProducts.waypoint(footerWaypoint, {offset:navHolder.height()+30});
+	
+	
+	
+	
 });
